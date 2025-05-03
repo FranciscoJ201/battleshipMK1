4091cd285af2b1d735654b24ef53661ded8f7fc2
FranciscoJ201 <francisco.jimenez40@qmail.cuny.edu>
2025-05-03 00:08:47 -0400

    fix choose moves

--- AIPlayer.py
+++ AIPlayer.py
@@ -1,16 +1,44 @@
 from Gameboard import GameBoard
 from Cellstate import CellState
+from boards import preset_board
+import random
+
 class AIPlayer:
     def __init__(self,board):
         self.board = board  # The real game board
-        self.target_stack = []
+        self.target_stack = [(0,0)]
         self.visited_moves = set()
 
     def choose_move(self):
-       
-       for i, row in enumerate(self.board.grid):
-                for j, val in enumerate(row):
-                    if self.board.grid[i][j] != CellState.HIT:
-                        self.board.grid[i][j] = CellState.HIT
+        #MAKE IT SO THAT IT ADDS TO TARGET STACK
+        while len(self.target_stack) > 0:
+            row,col = self.target_stack.pop()
+            if (self.board.is_valid_move(row,col)) and ((row,col) not in self.visited_moves):
+                
+                return row, col
+
+        #old
+
+        while True:
+            row, col = random.randint(0, self.board.size - 1), random.randint(0, self.board.size - 1)
+            if (self.board.is_valid_move(row,col)) and ((row,col) not in self.visited_moves):
+                
+                return row, col     
+
+
+# board = GameBoard()
+# board.grid = preset_board
+# board.display()
+# AI = AIPlayer(board)
+# # print(AI.choose_move())
+# # print(len(AI.target_stack))
+# AI.choose_move()
+
+
+
+    #    for i, row in enumerate(self.board.grid):
+    #             for j, val in enumerate(row):
+    #                 if self.board.grid[i][j] != CellState.HIT:
+    #                     self.board.grid[i][j] = CellState.HIT
                         
                         
--- Gameboard.py
+++ Gameboard.py
@@ -11,7 +11,14 @@ class GameBoard:
         self.grid[row][col] = CellState.MISS
 
     def is_valid_move(self, row, col):
-        return 0 <= row < self.size and 0 <= col < self.size and self.grid[row][col] not in (CellState.MISS or CellState.HIT)
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid)):
+                if (0<=row<10) and (0<=col<10) and (self.grid[row][col] in (CellState.EMPTY,CellState.SHIP)): #need this weird comparison for enumerate stuff
+                    return True
+        return False
+    
+
+        
     
     def all_hits_found(self):
         for row in range(len(self.grid)):
@@ -23,11 +30,11 @@ class GameBoard:
     def display(self):
         #count ship squares & check what type of board [Cells or ints] and convert
         if isinstance(self.grid[0][0], int):
-            for i, row in enumerate(self.grid):
-                for j, val in enumerate(row):
-                    if self.grid[i][j] == 3:
+            for row in range(len(self.grid)):
+                for col in range(len(self.grid)):
+                    if self.grid[row][col] == 3:
                         self.shipsqaures +=1
-                    self.grid[i][j] = CellState(val)
+                    self.grid[row][col] = CellState(self.grid[row][col])
         
         for row in self.grid:
             row_string = ''
