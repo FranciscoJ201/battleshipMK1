04e0064423197851369d1353876fbd946d8a641e
FranciscoJ201 <francisco.jimenez40@qmail.cuny.edu>
2025-05-08 13:31:23 -0400

    placeholder

--- AIPlayer.py
+++ AIPlayer.py
@@ -16,29 +16,39 @@ class AIPlayer:
             if (self.board.is_valid_move(row,col)) and ((row,col) not in self.visited_moves):
                 return row, col
 
-        
-        constRow = 3
-        while True:
-            # row, col = random.randint(0, self.board.size - 1), random.randint(0, self.board.size - 1)
-            #when it sinks a ship all the boxes around it become invalid (added to visited moves)
-            #in addition must add code that recognizes whole ship, and total number of ships (in board generator)
-
-            if (self.board.is_valid_move(row,col)) and ((row,col) not in self.visited_moves):
-                row, col = row + 1, col + 1
-                return row, col
+        row = 8 
+        col = 0
+        initial = 8
+        iterate = 0
+        done = False
+        while not done:
+            if row<10 and col < 10:
+                self.target_stack.append((row,col))
+                col += 1
+                row += 1
             else:
-                
-                return row - constRow, col
+                iterate+=2
+                row= initial-iterate 
+                col=0 
+                self.target_stack.append((row,col))
+            if row == 2 and col == 10:
+                done = True
 
 
-# board = GameBoard()
-# board.grid = preset_board
-# board.display()
-# AI = AIPlayer(board)
-# # print(AI.choose_move())
-# # print(len(AI.target_stack))
-# AI.choose_move()
+# row, col = random.randint(0, self.board.size - 1), random.randint(0, self.board.size - 1)
+#when it sinks a ship all the boxes around it become invalid (added to visited moves)
+#in addition must add code that recognizes whole ship, and total number of ships (in board generator)
 
+board = GameBoard()
+board.grid = preset_board
+board.display()
+AI = AIPlayer(board)
+# print(AI.choose_move())
+# print(len(AI.target_stack))
+n=0
+while n<30:
+    print(AI.choose_move())
+    n+=1
 
 
     #    for i, row in enumerate(self.board.grid):
--- check.py
+++ check.py
@@ -1,6 +1,30 @@
 grid = [[0 for i in range(10)] for j in range(10)] 
-for row in range(len(grid)):
-    print(grid[row])
-n = 0
-while n < 100:
-    row = 
+
+
+
+row = 8 
+col = 0
+initial = 8
+iterate = 0
+done = False
+while not done:
+    if row<10 and col < 10:
+        grid[row][col]=3
+        col += 1
+        row += 1
+    else:
+        iterate+=2
+        row= initial-iterate 
+        col=0 
+        grid[row][col]=3
+    if row == 2 and col == 10:
+        done = True
+    
+
+for row in grid:
+            row_string = ''
+            for col in row:
+                row_string += str(col) + ' '
+            print(row_string.strip())  #whitespace at end gets removed
+
+
